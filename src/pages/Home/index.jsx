bcc9aeb7a26d0d5ff50bebdc796bc86fe898751e
valerio-junior <valeriojuniornegocios@gmail.com>
2026-06-21 15:18:43 -0300

    Ajustar forms e validação campos senha e email

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,44 @@
-import { Container, Sugestao, ContainerEsquerda, ContainerLogo, ContainerInput, Input, Button } from "./style"
+import { Container, Sugestao, ContainerEsquerda, ContainerDireita, ContainerInput, Input, Button,Form } from "./style"
 
 import imageComp from "../../assets/img/image-comp.png"
 import logo from "../../assets/img/logo.png"
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+
 
 function Home() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [erro, setErro] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email.trim() || !senha.trim()) {
+      setErro('Por favor, preencha todos os campos!');
+      return; // Para a execução aqui e não deixa avançar
+    }
+
+    if (!email.includes('@')) {
+      setErro('Por favor, insira um e-mail válido (deve conter "@").');
+      return; // Para a execução aqui
+    }
+
+    // Se passou pelas duas validações acima, limpa o erro e limpa os campos
+    setErro('');
+
+    //redireciona para a próxima página do projeto
+    console.log('Campos validados com sucesso! Entrando...');
+    navigate('/details'); // Rota da pagina
+  };
 
   return (
     <Container>
-
+      
       <Sugestao>
         <a href="https://feedback.userreport.com/7275e80b-0459-4d2a-94d1-05c79f73478a/#ideas/popular" target='blank'>Deixe sua sugestão</a>
       </Sugestao>
@@ -16,20 +46,31 @@ function Home() {
         <img src={imageComp} alt="logo do londrisoft" />
       </ContainerEsquerda>
 
-      <ContainerLogo>
+      <ContainerDireita>
         < img src={logo} alt="logo do londrisoft" />
-        <ContainerInput>
-          <label htmlFor="email">E-mail</label>
-          <Input type='email' placeholder='email@provider.com'></Input>
-        </ContainerInput>
+        <Form onSubmit={handleLogin}>
+          <ContainerInput>
+            <label htmlFor="email">E-mail</label>
+            <Input type='text' placeholder='email@provider.com' value={email} onChange={(e) => setEmail(e.target.value)}>
 
-        <ContainerInput>
-          <label htmlFor="senha">Senha</label>
-          <Input type='password' placeholder='********'></Input>
-        </ContainerInput>
-        <Button>Acessar</Button>
-      </ContainerLogo>
-      
+            </Input>
+          </ContainerInput>
+          <ContainerInput>
+            <label htmlFor="senha">Senha</label>
+            <Input type='password' placeholder='********' value={senha} onChange={(e) => setSenha(e.target.value)}></Input>
+          </ContainerInput>
+          <Button type="submit">Acessar</Button>
+
+        </Form>
+
+        {/* 4. Exibe a mensagem de erro na tela apenas se o estado 'erro' não estiver vazio */}
+      {erro && (
+        <p style={{ color: '#ff3333', fontWeight: 'bold', marginBottom: '15px' }}>
+          {erro}
+        </p>
+      )}
+      </ContainerDireita>
+
     </Container>
 
   )
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,6 +4,7 @@ export const Container = styled.div `
     height: 100vh;
     overflow: hidden;
     display: flex;
+    gap: 80px;
 
     @media(max-width: 998px) {
         justify-content: center;
@@ -53,7 +54,7 @@ export const ContainerEsquerda = styled.div `
 
 `
 
-export const ContainerLogo = styled.div `
+export const ContainerDireita = styled.div `
     width: 100vh;
     display: flex;
     flex-direction: column;
@@ -62,9 +63,16 @@ export const ContainerLogo = styled.div `
     gap: 20px;
 
     img {
-        margin: 0 0 12px 12px;
+        margin: 0 0 8px 8px;
     }
-
+`
+export const Form = styled.form `
+   
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
 `
 
 export const ContainerInput = styled.div `
@@ -72,6 +80,7 @@ export const ContainerInput = styled.div `
     display: flex;
     flex-direction: column;
     label {
+        margin-bottom: 5px;
         font-size: 0.75rem;
         color: rgb(143, 143, 143);
     }
@@ -80,10 +89,10 @@ export const ContainerInput = styled.div `
 
 export const Input = styled.input `
     outline: transparent solid 2px;
-    padding: 10px;
+    padding: 10px 13px;
     border-radius: 4px;
     border: 1px solid #F0F0F0;
-    width: 400px;
+    width: 350px;
     background-color: #F5F5F5;
     font-size: 0.8rem;
 
@@ -95,10 +104,10 @@ export const Input = styled.input `
 export const Button = styled.button `
     margin-top: 0.2rem;
     width: 150px;
-    padding: 10px;
+    padding: 12px;
     border-radius: 5px;
     border: none;
-    background-color: #166DD7;
+    background-color: #1155BB;
     color: #FFFF;
     cursor: pointer;
     &:hover {
